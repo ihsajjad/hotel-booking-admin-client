fc3a8f25df6079b213c9f3c6822c3f4f1e6a2ff6
ihsajjad <ihsajjad1@gmail.com>
2023-09-05 10:31:35 +0600

    completed login page design

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,5 +1,26 @@
 const Login = () => {
-  return <div className="login-container"></div>;
+  return (
+    <div className="login-container">
+      <div className="form-container">
+        <h3>Please Login!</h3>
+        <form>
+          <div>
+            <label htmlFor="email-input">Email:</label>
+            <input type="email" name="email" id="email-input" />
+          </div>
+          <div>
+            <label htmlFor="">Password:</label>
+            <input type="password" name="password" id="password-input" />
+          </div>
+          <input type="submit" value="Login" />
+        </form>
+
+        <div className="social-login">
+          <button>G</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
