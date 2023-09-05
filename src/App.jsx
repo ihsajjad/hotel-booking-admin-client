13f9710115e3fc6db3c2bf55d669bee159189d66
ihsajjad <ihsajjad1@gmail.com>
2023-09-05 10:01:40 +0600

    Initial setup done

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import "./App.css";
+
+function App() {
+  console.log(import.meta.env.VITE_apiKey);
+  return <main className="main-container"></main>;
+}
+
+export default App;
---
+++ src/Login.jsx
@@ -0,0 +1,5 @@
+const Login = () => {
+  return <div className="login-container"></div>;
+};
+
+export default Login;
